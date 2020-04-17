78a8f44b0dead00957593a2c8795cc3c06aec244
kosarchuksn <sergeykosarchuk@gmail.com>
2020-04-17 16:30:24 +0300

    Include app label in get change attachment path

--- django_documents_tools/utils.py
+++ django_documents_tools/utils.py
@@ -2,5 +2,6 @@ import os
 
 
 def get_change_attachment_path(instance, file_name):
+    app_label = instance._meta.app_label  # noqa: protected-access
     model_name = instance._meta.model_name  # noqa: protected-access
-    return os.path.join(model_name, file_name)
+    return os.path.join(app_label, model_name, file_name)
