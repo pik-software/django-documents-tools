8313e4c934f7408e16cd9c897f3158ae5af4c92b
zenin.ds <zenin.ds@pik.ru>
2022-07-11 17:33:04 +0300

    replace __range with __date

--- django_documents_tools/tasks.py
+++ django_documents_tools/tasks.py
@@ -15,13 +15,11 @@ class StartTimeTask(Task):  # noqa: abstract-method
 def apply_postponed_documents(
         model_names: Iterable[str], start_time: str):
     today = datetime.fromisoformat(start_time).date()
-    start_today = datetime.combine(today, datetime.min.time())
-    end_today = start_today + timedelta(days=1)
     for model_str in model_names:
         app_label, model_name = model_str.split('.')
         model = apps.get_model(app_label=app_label, model_name=model_name)
         documents_qs = model.changes.filter(
-            document_date__range=[start_today, end_today])
+            document_date__date=today, document_is_draft=False)
         for document in documents_qs:
             if document.building.changes.apply_to_object():
                 document.building.save()
